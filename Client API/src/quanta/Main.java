61bbcc7359ff59dd7e43e420b10a723ede67a83a
Marijn van Wezel <lmnkgames4@gmail.com>
2015-05-25 19:33:25 +0200

    ClassBody fix.

--- Client API/src/quanta/Main.java
+++ Client API/src/quanta/Main.java
@@ -14,4 +14,5 @@ public class Main {
 	public static void main(String... args) {
 		net.minecraft.client.main.Main.main(new String[] {"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}", Arrays.toString(args)});
 	}
+	
 }
