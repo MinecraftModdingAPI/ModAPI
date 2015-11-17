780ff0b12409e0b36fed3cbbe8911b726141f397
link_the_programmer <linktheprogrammer@gmail.com>
2015-11-16 22:34:58 -0500

    Initial refactor

---
+++ src/api/quantum/meta/WIP.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api.quantum.meta;
+
+/**
+ * Shorthand for the Annotation {@code @WorkInProgress}.
+ * 
+ * 
+ * @author link
+ */
+@WorkInProgress
+public @interface WIP {
+    /* No need to do stuff here, everything is inherited */
+}
---
+++ src/api/quantum/meta/WorkInProgress.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api.quantum.meta;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author link
+ */
+@Target({ElementType.CONSTRUCTOR, ElementType.PACKAGE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+public @interface WorkInProgress {
+
+}
