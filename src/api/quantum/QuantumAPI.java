32f587435e3b0bd978bb22f26af8bd75ef7348e5
link_the_programmer <linktheprogrammer@gmail.com>
2015-11-16 22:01:13 -0500

    Initial refactor

---
+++ src/api/quantum/QuantumAPI.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api.quantum;
+
+/**
+ *
+ * @author link
+ */
+public class QuantumAPI {
+    /* 
+     * This exists for a reason. that reason, I currently do not know. 
+     * I have a vague idea of what i'm going to do with this. Probably
+     * going to be an info class. 
+     */
+}
